230a0ff1f03b540534dcfad5a791ba6920c1cc29
Aayush Patil <65178660+Aayushpatil77@users.noreply.github.com>
2023-05-29 23:48:59 +0530

    changed aws keys for vercel

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -22,8 +22,8 @@ function addCountryCode(number) {
 
 const region = "ap-south-1";
 const bucketName = "telephony-bot";
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const accessKeyId = process.env.LOCAL_AWS_ACCESS_KEY;
+const secretAccessKey = process.env.LOCAL_AWS_SECRET_KEY;
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
